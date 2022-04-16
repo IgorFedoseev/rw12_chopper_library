3b98f4e04a7089cd0ca6646c29f9543e28de8e5e
Igor Fedoseev <ifedoseev.flagman@gmail.com>
2022-04-16 15:33:05 +0300

    recipe details edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
+import 'package:provider/provider.dart';
 
+import 'data/memory_repository.dart';
 import 'ui/main_screen.dart';
 
 Future<void> main() async {
@@ -10,7 +12,7 @@ Future<void> main() async {
 }
 
 // 7) add method for logging requests and responses
-void _setupLogging(){
+void _setupLogging() {
   Logger.root.level = Level.ALL;
   // try changing ALL to WARNING, SEVERE...
   Logger.root.onRecord.listen((rec) {
@@ -21,19 +23,22 @@ void _setupLogging(){
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Recipes',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+    return ChangeNotifierProvider<MemoryRepository>(
+      lazy: false,
+      create: (_) => MemoryRepository(),
+      child: MaterialApp(
+        title: 'Recipes',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
           brightness: Brightness.light,
           primaryColor: Colors.white,
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const MainScreen(),
       ),
-      home: const MainScreen(),
     );
   }
 }
